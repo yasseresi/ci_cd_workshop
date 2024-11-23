d1926bd068a974e6925038790865ac444131a922
YASSER Korzane <ly_korzane@esi.dz>
2024-11-23 16:10:40 +0100

    modifing the / endpoint

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello, CI/CD!');
+    res.send('Hi Bytecraft !');
 });
 
 app.get('/greet', (req, res) => {
